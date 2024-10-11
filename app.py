65a7aa57353c7cfea9e43af28bd2eeff57530874
Paul Camus <priorifatin@gmail.com>
2024-10-11 00:37:21 -0300

    Quinto commit, organizando as rotas de forma correta

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ def main(page: ft.Page):
     # Função para gerenciar as rotas
     def on_route_change(e):
         page.views.clear()  # Limpa as views para adicionar a nova tela
+
+        # Tela de login
         if page.route == "/login":
             # Definindo o título da página
             page.title = "PrepWise"
@@ -41,6 +43,60 @@ def main(page: ft.Page):
                     [
                         
                         ft.Text('Login', size=30, weight="bold"),
+
+                        # Campo de email
+                        ft.TextField(
+                            label="Informe seu email", 
+                            text_align=ft.TextAlign.LEFT,
+                            text_style=ft.TextStyle(size=15, weight="bold"),
+                            width=310,
+                            border_radius=10,
+                            color="Blue",
+                        ),
+                        
+                        
+                        # Adiciona a linha com o campo de senha e o ícone
+                        senha_row,
+
+                        # Botão de entrar redirecionando para /tela_inicial
+                        ft.ElevatedButton("Entrar", on_click=lambda _: page.go("/tela_inicial")),
+
+                        
+                        ft.ElevatedButton("Não possui cadastro?", on_click=lambda _: page.go("/tela_cadastro")),
+                    ],
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    vertical_alignment=ft.MainAxisAlignment.CENTER,
+                )
+            )
+        #Tela Inicial
+        elif page.route == "/tela_inicial":
+            # Tela inicial
+            page.views.append(
+                ft.View(
+                    "/tela_inicial",
+                    [
+                        ft.Text("Bem-vindo ao PrepWise", size=30, weight="bold"),
+
+                        ft.ElevatedButton(
+                            "Ir para Login", 
+                            on_click=lambda _: page.go("/login"),
+                        ),
+                    ],
+                    #horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    #vertical_alignment=ft.MainAxisAlignment.CENTER,
+                )
+            )
+        elif page.route == "/tela_cadastro":
+            ft.title = "PrepWise - Cadastro"
+            senha_row = ft.Row(
+                controls=[txt_senha, icon_eye],  # Coloca o campo de senha e o ícone na mesma linha   
+                alignment=ft.MainAxisAlignment.CENTER
+            )
+            #tela de cadastro
+            page.views.append(
+                ft.View(
+                    "/tela_cadastro",[
+                        ft.Text("Tela de cadastro"),
 
                         # Campo de nome completo
                         ft.TextField(
@@ -61,28 +117,16 @@ def main(page: ft.Page):
                             color="Blue",
                         ),
                         
-                        
                         # Adiciona a linha com o campo de senha e o ícone
                         senha_row,
 
                         # Botão de entrar redirecionando para /tela_inicial
-                        ft.ElevatedButton("Entrar", on_click=lambda _: page.go("/tela_inicial")),
+                        ft.ElevatedButton("Cadastrar", on_click=lambda _: page.go("/tela_inicial")),
                     ],
                     horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                     vertical_alignment=ft.MainAxisAlignment.CENTER,
-                )
-            )
-        elif page.route == "/tela_inicial":
-            # Tela inicial
-            page.views.append(
-                ft.View(
-                    "/tela_inicial",
-                    [
-                        ft.Text("Bem-vindo à tela inicial", size=30, weight="bold"),
-                        ft.ElevatedButton("Ir para Login", on_click=lambda _: page.go("/login"))
-                    ],
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    vertical_alignment=ft.MainAxisAlignment.CENTER,
+
+                    
                 )
             )
         page.update()  # Atualiza a página com a nova view
@@ -90,6 +134,6 @@ def main(page: ft.Page):
     page.on_route_change = on_route_change
 
     # Defina a rota inicial (primeira tela)
-    page.go("/login")
+    page.go("/tela_inicial")
 
 ft.app(target=main)
